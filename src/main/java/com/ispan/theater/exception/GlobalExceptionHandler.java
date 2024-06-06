dad6622c5c17b4b5f3e1e1879294f02ac846309c
LiuHouDe <pqq1234562002@gmail.com>
2024-06-06 10:14:17 +0800

    0606

--- src/main/java/com/ispan/theater/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ispan/theater/exception/GlobalExceptionHandler.java
@@ -16,29 +16,41 @@ import io.jsonwebtoken.ExpiredJwtException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ErrorDto> handleException(Exception e){
-		System.out.println(e.getMessage());
-		System.out.println(e.getStackTrace());
-		System.out.println(e.getCause());
-		System.out.println(e.getClass().getName());
-		return ResponseEntity.status(HttpStatus.SC_INTERNAL_SERVER_ERROR).body(new ErrorDto(DatetimeConverter.createSqlDatetime(new Date()),HttpStatus.SC_INTERNAL_SERVER_ERROR,"INTERNAL",e.getMessage()));
-	}
-	
-	@ExceptionHandler(ExpiredJwtException.class)
-	public ResponseEntity<ErrorDto> handleJwtException(ExpiredJwtException e){
-		return ResponseEntity.status(HttpStatus.SC_FORBIDDEN).body(new ErrorDto(DatetimeConverter.createSqlDatetime(new Date()),HttpStatus.SC_FORBIDDEN,"forbidden",e.getMessage()));
-	}
-	
-	@ExceptionHandler(OrderException.class)
-	public ResponseEntity<ErrorDto> handleOrderException(OrderException e){
-		return ResponseEntity.status(e.getErrorCode()).body(new ErrorDto(DatetimeConverter.createSqlDatetime(new Date()),e.getErrorCode(),"order_ticket_error",e.getErrorMessage()));
-	}
-	@ExceptionHandler(EcpayException.class)
-	public ResponseEntity<ErrorDto> handleEcpayException(EcpayException e){
-		e.ShowExceptionMessage();
-		return ResponseEntity.status(HttpStatus.SC_METHOD_FAILURE).body(new ErrorDto(DatetimeConverter.createSqlDatetime(new Date()),HttpStatus.SC_METHOD_FAILURE,"method_failure",e.getNewExceptionMessage()));
-	}
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDto> handleException(Exception e) {
+        System.out.println(e.getMessage());
+        System.out.println(e.getStackTrace());
+        System.out.println(e.getCause());
+        System.out.println(e.getClass().getName());
+        return ResponseEntity.status(HttpStatus.SC_INTERNAL_SERVER_ERROR).body(new ErrorDto(DatetimeConverter.createSqlDatetime(new Date()), HttpStatus.SC_INTERNAL_SERVER_ERROR, "INTERNAL", e.getMessage()));
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ErrorDto> handleJwtException(ExpiredJwtException e) {
+        System.out.println(e.getMessage());
+        System.out.println(e.getStackTrace());
+        System.out.println(e.getCause());
+        System.out.println(e.getClass().getName());
+        return ResponseEntity.status(HttpStatus.SC_FORBIDDEN).body(new ErrorDto(DatetimeConverter.createSqlDatetime(new Date()), HttpStatus.SC_FORBIDDEN, "forbidden", e.getMessage()));
+    }
+
+    @ExceptionHandler(OrderException.class)
+    public ResponseEntity<ErrorDto> handleOrderException(OrderException e) {
+        System.out.println(e.getMessage());
+        System.out.println(e.getStackTrace());
+        System.out.println(e.getCause());
+        System.out.println(e.getClass().getName());
+        return ResponseEntity.status(e.getErrorCode()).body(new ErrorDto(DatetimeConverter.createSqlDatetime(new Date()), e.getErrorCode(), "order_ticket_error", e.getErrorMessage()));
+    }
+
+    @ExceptionHandler(EcpayException.class)
+    public ResponseEntity<ErrorDto> handleEcpayException(EcpayException e) {
+        System.out.println(e.getMessage());
+        System.out.println(e.getStackTrace());
+        System.out.println(e.getCause());
+        System.out.println(e.getClass().getName());
+        return ResponseEntity.status(HttpStatus.SC_METHOD_FAILURE).body(new ErrorDto(DatetimeConverter.createSqlDatetime(new Date()), HttpStatus.SC_METHOD_FAILURE, "method_failure", e.getNewExceptionMessage()));
+    }
 
 }
